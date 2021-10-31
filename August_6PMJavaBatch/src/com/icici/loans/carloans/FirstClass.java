a412ba715f74e85126ffa6c6475337aeb987c5ed
vsanaboina <70114320+vsanaboina@users.noreply.github.com>
2021-10-31 00:42:48 -0400

    added variables in a,b,c in Pune Branch.

--- August_6PMJavaBatch/src/com/icici/loans/carloans/FirstClass.java
+++ August_6PMJavaBatch/src/com/icici/loans/carloans/FirstClass.java
@@ -1,11 +1,12 @@
 package com.icici.loans.carloans;
 
 public class FirstClass {
-
+int a=10, b=20, c=30;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hi !");
      System.out.println("GIT commit purpose  10302021");
+     
      
 	}
 
