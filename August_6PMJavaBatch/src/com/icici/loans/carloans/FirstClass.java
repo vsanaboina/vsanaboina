e2693ddead283324d2ad1459d11d8da8fa12872f
vsanaboina <70114320+vsanaboina@users.noreply.github.com>
2021-10-31 00:52:56 -0400

    added x,y,z variabels to the Hyd branch

--- August_6PMJavaBatch/src/com/icici/loans/carloans/FirstClass.java
+++ August_6PMJavaBatch/src/com/icici/loans/carloans/FirstClass.java
@@ -1,7 +1,7 @@
 package com.icici.loans.carloans;
 
 public class FirstClass {
-
+int x=100, y=200, z=300;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hi !");
