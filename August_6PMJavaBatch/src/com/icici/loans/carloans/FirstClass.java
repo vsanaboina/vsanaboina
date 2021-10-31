733e26222010e66a5d1fee587ce2c778fa7d30a4
vsanaboina <70114320+vsanaboina@users.noreply.github.com>
2021-10-31 00:26:57 -0400

    reverted the code

--- August_6PMJavaBatch/src/com/icici/loans/carloans/FirstClass.java
+++ August_6PMJavaBatch/src/com/icici/loans/carloans/FirstClass.java
@@ -5,7 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hi !");
-     System.out.println("GIT commit purpose 10302021");
+     System.out.println("GIT commit purpose  10302021");
+     
 	}
 
 }
